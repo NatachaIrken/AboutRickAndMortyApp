21877b0db51095d47be0f444fe79f9aaecae0af3
Victoria Saldaña <victoria.saldana@bci.cl>
2024-08-13 23:37:42 -0400

    Create BaseUrl.swift

---
+++ AboutRickAndMorty/Data/Service/BaseUrl.swift
@@ -0,0 +1,13 @@
+//
+//  EndPoints.swift
+//  AboutRickAndMorty
+//
+//  Created by Natacha Saldaña on 13-08-24.
+//
+
+import Foundation
+
+public class BaseUrl {
+	public let urlBase = "https://rickandmortyapi.com"
+	public let endPoint = "/api/character"
+}
