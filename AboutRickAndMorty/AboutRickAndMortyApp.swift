fcab2251a8490cddf430c377aeafd1014634d24f
Victoria Saldaña <victoria.saldana@bci.cl>
2024-08-13 22:16:38 -0400

    Create project and folders

---
+++ AboutRickAndMorty/AboutRickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutRickAndMortyApp.swift
+//  AboutRickAndMorty
+//
+//  Created by Natacha Saldaña on 13-08-24.
+//
+
+import SwiftUI
+
+@main
+struct AboutRickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutRickAndMorty/Data/Service/EndPoints.swift
@@ -0,0 +1,13 @@
+//
+//  EndPoints.swift
+//  AboutRickAndMorty
+//
+//  Created by Natacha Saldaña on 13-08-24.
+//
+
+import Foundation
+
+class BaseUrl {
+	let urlBase = ""
+	let endPoint = "" 
+}
